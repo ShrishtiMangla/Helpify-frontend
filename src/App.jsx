0e5c9738b357f65f3575193c7b5f13c0d210c1ae
shrishtimangla <shrishtimangla9@gmail.com>
2025-12-15 17:25:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3 class="text-xl font-semibold text-gray-700">
+        Recent Conversations
+      </h3>
+
+    </>
+  )
+}
+
+export default App
